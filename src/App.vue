3f2d7b40f523eb600cfe8e7133c03741ec0ccbd1
Artist <1443885711@qq.com>
2021-11-29 16:01:12 +0800

    third commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <amap :zoom="15" :center="position">
+      <amap-marker
+          :position="position"
+          :label="{
+          content: 'Hello, AMap-Vue!',
+          direction: 'bottom',
+        }"
+          draggable
+      />
+    </amap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      position: [116.473179, 39.993169]
+    };
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+</style>
+
+<style lang="less" scoped>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+import AmapVue from "@amap/amap-vue";
+
+Vue.config.productionTip = false;
+
+AmapVue.config.key = "72270b16a1fc514d607e013124e7ca61";
+Vue.use(AmapVue);
+
+new Vue({
+    render: h => h(App)
+}).$mount("#app");
